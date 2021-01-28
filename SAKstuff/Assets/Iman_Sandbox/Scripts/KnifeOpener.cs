2e892de02068a5f1a69931134db434bef4cc99a5
imannikk <61586634+imannikk@users.noreply.github.com>
2021-01-28 16:43:32 +0100

    02-Iman assets

---
+++ SAKstuff/Assets/Iman_Sandbox/Scripts/KnifeOpener.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnifeOpener : MonoBehaviour
+{
+    public Animator animator;
+
+    void Update()
+    {
+        // ask animator state then change it to another state and then set it and store it again.
+        if(Input.GetKeyDown(KeyCode.O))
+        {
+            bool open = animator.GetBool("open");
+            open = !open;
+            animator.SetBool("open", open);
+        }
+    }
+}
