530811a5407478237231a6f244e81af4d757581b
NamgarM <50959596+NamgarM@users.noreply.github.com>
2021-01-30 22:28:56 +0100

    1) Flipping

---
+++ SAKstuff/Assets/NM/Scripts_NM/Main room/FlippingAnimationButton.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+/// <summary>
+/// P3 Swiss Army Knife project
+/// Namgar Mardvaeva
+/// Group № 2 (Iman, Namgar, Torben)
+/// Summary: 
+/// </summary>
+
+public class FlippingAnimationButton : MonoBehaviour
+{
+    private bool open = false;
+    // Observer
+    public UnityEvent flippingAnimationButtonPressed;
+
+    // For testing without vr controllers
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            Debug.Log("Flipping starts");
+            flippingAnimationButtonPressed.Occured();
+        }
+    }
+
+    // Is this to access to steam vr?
+    public void OnPress(Handheld hand)
+    {
+        Debug.Log("SteamVR Button was pressed");
+    }
+
+    // Flipping in VR
+    public void OnCustomButtonPress()
+    {
+        if (open == false)
+        {
+            // Observer
+            flippingAnimationButtonPressed.Occured();
+        } else
+        {
+            //flippingAnimationButtonPressed.Reoccured???(); reocurred and then access to another animation
+            // or unregister??? but then it will just delete observer from list, and it is wrong for us because we need to re-use
+        }
+    }
+}
---
+++ SAKstuff/Assets/NM/Scripts_NM/Main room/SAKMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SAKMovement : MonoBehaviour
+{
+    private Animator SAKanim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SAKanim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //AnimatorStateInfo stateInfo = SAKanim.GetCurrentAnimatorStateInfo(0);
+    }
+
+    public void ButtonPressed()
+    {
+        Flip();
+    }
+
+    public void ButtonRepressed()
+    {
+        Unflip();
+    }
+
+    public void Flip()
+    {
+        Debug.Log("Flipped");
+        SAKanim.SetBool("Open", true);
+    }
+
+    public void Unflip()
+    {
+        Debug.Log("Unflipped");
+        SAKanim.SetBool("Open", false);
+    }
+}
