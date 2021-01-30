e4e4e9fa6d9ed80f06d3f40bd77e05dd8eb7d790
NamgarM <50959596+NamgarM@users.noreply.github.com>
2021-01-30 21:55:11 +0100

    1) Button for flipping

---
+++ SAKstuff/Assets/NM/Scripts_NM/Main room/With DIP/UnityEvent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Event", menuName = "Unity Event", order = 52)]
+public class UnityEvent : ScriptableObject
+{
+    private List<EventObserver> eobservers = new List<EventObserver>();
+
+    public void Register(EventObserver observer)
+    {
+        eobservers.Add(observer);
+    }
+
+    public void Unregister(EventObserver observer)
+    {
+        eobservers.Remove(observer);
+    }
+
+    // Looping throught registered observers and triggers the method
+    public void Occured()
+    {
+        for (int counter = 0; counter < eobservers.Count; counter++)
+        {
+            eobservers[counter].OnEventOccurs();
+        }
+    }
+
+    public void Reoccured()
+    {
+        for (int counter = 0; counter < eobservers.Count; counter++)
+        {
+            eobservers[counter].OnEventReoccurs();
+        }
+    }
+
+}
---
+++ SAKstuff/Assets/NM/Scripts_NM/SAKActivation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SAKActivation : MonoBehaviour
+{
+    private bool start = false;
+
+    public void Activate()
+    {
+        Debug.Log("It starts");
+        start = true;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(start == true || Input.GetKeyDown(KeyCode.Z))
+        {
+            Debug.Log("We are ready");
+        }
+    }
+}
