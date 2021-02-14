6d01115e449dd88ef15de03fd16397939937cd30
skysmiter <glo_age@hotmail.com>
2021-02-14 18:24:41 +0100

    15 Story Lvl Adjustments

---
+++ SAKstuff/Assets/TorbenThings/BridgeAnim.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeAnim : MonoBehaviour
+{
+    public CutRope cutRopeScript;
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (cutRopeScript.ropeCut)
+        {
+            anim.SetTrigger("fallBridge");
+        }
+    }
+}
