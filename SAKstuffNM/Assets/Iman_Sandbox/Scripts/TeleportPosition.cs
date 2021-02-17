7a7992a3971807287aa9cf1ebd28d6874da9bda8
NamgarM <50959596+NamgarM@users.noreply.github.com>
2021-02-17 20:09:33 +0100

    Deleted

--- SAKstuffNM/Assets/Iman_Sandbox/Scripts/TeleportPosition.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TeleportPosition : MonoBehaviour
-{
-    public int index;
-    public bool initial;
-}
